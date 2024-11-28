f77dfbea3a3d4540b2e39cbbd9265fc18fe90e73
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-11-28 22:30:17 +0530

    Add Font Family In Index

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        zentry: ["zentry", "sanf-serif"],
+        general: ["general", "sanf-serif"],
+        "circular-web": ["circular-web", "sanf-serif"],
+        "robert-medium": ["robert-medium", "sanf-serif"],
+        "robert-regular": ["robert-regular", "sanf-serif"],
+      },
+    },
   },
   plugins: [],
 };
